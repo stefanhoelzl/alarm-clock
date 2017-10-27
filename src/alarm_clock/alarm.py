889b189b45a5e35a7c203b35a53b9deadbd135aa
Stefan Hölzl <stefan.hoelzl@gmx.de>
2017-10-27 21:49:04 +0200

    support for repeating on several days

--- src/alarm_clock/alarm.py
+++ src/alarm_clock/alarm.py
@@ -9,15 +9,39 @@ def seconds(hours, minutes):
 
 
 class Alarm:
+    YEAR = 0
+    MONTH = 1
+    DAY = 2
+    HOUR = 3
+    MINUTE = 4
+    SECOND = 5
+    WEEKDAY = 6
+    YEARDAY = 7
+
+    all = []
+
+    @classmethod
+    def activated(cls, h, m, days=None, daylight_time=None):
+        alarm = Alarm()
+        alarm.hour = h
+        alarm.minute = m
+        alarm.days = days
+        alarm.daylight_time = daylight_time
+        alarm.activate()
+        return alarm
+
     def __init__(self):
-        self.enabled = True
+        self.enabled = False
         self.daylight_time = None
         self.snooze_time = 0
         self.hour = 0
         self.minute = 0
+        self.days = None
 
         self.time = 0
         self.snooze_counter = 0
+
+        Alarm.all.append(self)
 
     @property
     def daylight(self):
@@ -43,18 +67,50 @@ class Alarm:
     def set_time(self):
         current_time = time.time()
         t = list(time.localtime(current_time))
-        t[3] = self.hour
-        t[4] = self.minute
+        t[self.HOUR] = self.hour
+        t[self.MINUTE] = self.minute
+        t[self.SECOND] = 0
         alarm_time = time.mktime(tuple(t))
         if alarm_time <= current_time:
             alarm_time += seconds(24, 0)
         self.time = alarm_time
 
+    @staticmethod
+    def diff_between_weekdays(day0, day1):
+        diff = day1 - day0
+        if diff < 0:
+            diff = (day1 + 7) - day0
+        return diff
+
+    def get_next_weekday(self, current_day):
+        for day in sorted(self.days):
+            if day > current_day:
+                return day
+        return min(self.days)
+
+    def next_time(self):
+        if not self.days:
+            return None
+
+        if self.time > time.time():
+            return self.time
+
+        t = list(time.localtime(self.time))
+        current_day = t[self.WEEKDAY]
+        next_day = self.get_next_weekday(current_day)
+
+        diff = self.diff_between_weekdays(current_day, next_day)
+        return 24*diff*60*60+self.time
+
     def snooze(self):
         self.snooze_counter += 1
 
     def off(self):
-        self.enabled = False
+        if self.days:
+            self.snooze_counter = 0
+            self.time = self.next_time()
+        else:
+            self.deactivate()
 
     def activate(self):
         self.set_time()
--- tests/test_alarm.py
+++ tests/test_alarm.py
@@ -1,6 +1,11 @@
+import pytest
 from unittest.mock import patch, PropertyMock
 
 from alarm_clock.alarm import Alarm
+
+
+def time(h, m):
+    return ((h-1)*60 + m)*60
 
 
 class TestAlarm:
@@ -13,6 +18,7 @@ class TestAlarm:
            new_callable=PropertyMock, return_value=0)
     def test_daylight_onZeroTimeLeft_daylightFull(self, t):
         alarm = Alarm()
+        alarm.enabled = True
         alarm.daylight_time = 1
         assert alarm.daylight == 1.0
 
@@ -25,6 +31,7 @@ class TestAlarm:
 
     def test_daylight_onTimeLeftBetweenDayligthTimeAnd0_daylightDim(self):
         alarm = Alarm()
+        alarm.enabled = True
         alarm.daylight_time = 100
         for time_left in range(100, 0, -1):
             with patch(__name__ + '.Alarm.time_left',
@@ -65,12 +72,16 @@ class TestAlarm:
     @patch(__name__ + '.Alarm.time_left',
            new_callable=PropertyMock, return_value=0)
     def test_ringing_timeLeftIsZero_True(self, t):
-        assert Alarm().ringing
+        alarm = Alarm()
+        alarm.enabled = True
+        assert alarm.ringing
 
     @patch(__name__ + '.Alarm.time_left',
            new_callable=PropertyMock, return_value=-0.1)
     def test_ringing_timeLeftLessThanZero_True(self, t):
-        assert Alarm().ringing
+        alarm = Alarm()
+        alarm.enabled = True
+        assert alarm.ringing
 
     @patch("time.time", return_value=1)
     @patch(__name__ + '.Alarm.snoozing',
@@ -127,21 +138,44 @@ class TestAlarm:
         alarm.off()
         assert not alarm.enabled
 
-    @patch("time.time", return_value=(16*60)*60)
+    def test_off_onRepeating_activate(self):
+        alarm = Alarm()
+        alarm.days = (1,)
+        alarm.enabled = True
+        alarm.off()
+        assert alarm.enabled
+
+    def test_off_onRepeating_setTime(self):
+        alarm = Alarm()
+        alarm.days = (1, 2, 3)
+        alarm.time = 60*60
+        with patch(__name__ + ".Alarm.next_time", return_value=24*60*60):
+            alarm.off()
+        assert alarm.time == 24*60*60
+
+    @patch("time.time", return_value=time(17, 0))
     def test_setTime_laterAtSameDay(self, t):
         alarm = Alarm()
         alarm.hour = 17
         alarm.minute = 10
         alarm.set_time()
-        assert alarm.time == (16*60+10)*60
+        assert alarm.time == time(17, 10)
 
-    @patch("time.time", return_value=(16*60+11)*60)
+    @patch("time.time", return_value=time(17, 10))
     def test_setTime_nextDay(self, t):
         alarm = Alarm()
         alarm.hour = 17
         alarm.minute = 10
         alarm.set_time()
-        assert alarm.time == ((24+16)*60+10)*60
+        assert alarm.time == time(24+17, 10)
+
+    @patch("time.time", return_value=time(17, 10)+5)
+    def test_setTime_secondsToZero(self, t):
+        alarm = Alarm()
+        alarm.hour = 17
+        alarm.minute = 10
+        alarm.set_time()
+        assert alarm.time == time(24+17, 10)
 
     def test_activate_setEnabled(self):
         alarm = Alarm()
@@ -161,10 +195,78 @@ class TestAlarm:
         alarm.hour = 17
         alarm.minute = 10
         alarm.activate()
-        assert alarm.time == (16*60+10)*60
+        assert alarm.time == time(17, 10)
 
     def test_deactivate_unsetEnabled(self):
         alarm = Alarm()
         alarm.enabled = True
         alarm.deactivate()
         assert not alarm.enabled
+
+    def test_nextTime_returnTime_ifTimeWasNotReached(self):
+        alarm = Alarm()
+        alarm.time = 100
+        alarm.days = (0,)
+        with patch("time.time", return_value=0):
+            assert alarm.next_time() == 100
+
+    def test_nextTime_returnNone_ifNoneDays(self):
+        alarm = Alarm()
+        assert alarm.next_time() is None
+
+    @pytest.mark.parametrize("current_day, days, day_diff", (
+            # single day
+            (0, (1,), 1),
+            (1, (2,), 1),
+            (6, (0,), 1),
+            (0, (6,), 6),
+            (4, (1,), 4),
+            (6, (0,), 1),
+            # multiple days
+            (3, (1, 3, 4), 1),
+            (4, (1, 4), 4),
+            # unordered days
+            (4, (4, 1), 4),
+    ))
+    def test_nextTime_onlyOneDayInSelection(self,
+                                            current_day, days, day_diff):
+        alarm = Alarm()
+        alarm.hour = 10
+        alarm.minute = 0
+        alarm.days = days
+        with patch("time.time", return_value=0):
+            alarm.set_time()
+        with patch("time.localtime",
+                   return_value=(0, 0, 0, 10, 0, 0, current_day, 0)):
+            assert alarm.next_time() == time(day_diff*24+10, 0)
+
+    def test_diffBetweenWeekdays_withoutOverflow(self):
+        assert Alarm.diff_between_weekdays(0, 2) == 2
+
+    def test_diffBetweenWeekdays_withOverflow(self):
+        assert Alarm.diff_between_weekdays(4, 1) == 4
+
+    def test_getNextWeekday_singleValueInList(self):
+        alarm = Alarm()
+        alarm.days = (1,)
+        assert alarm.get_next_weekday(2) == 1
+
+    def test_getNextWeekday_multipleOrderedDays(self):
+        alarm = Alarm()
+        alarm.days = (1, 4)
+        assert alarm.get_next_weekday(1) == 4
+
+    def test_getNextWeekday_multipleOrderedDaysWithOverflow(self):
+        alarm = Alarm()
+        alarm.days = (1, 4)
+        assert alarm.get_next_weekday(4) == 1
+
+    def test_getNextWeekday_multipleUnorderedDays(self):
+        alarm = Alarm()
+        alarm.days = (4, 1, 3)
+        assert alarm.get_next_weekday(3) == 4
+
+    def test_getNextWeekday_multipleUnorderedDaysWithOverflow(self):
+        alarm = Alarm()
+        alarm.days = (4, 1, 3)
+        assert alarm.get_next_weekday(4) == 1
