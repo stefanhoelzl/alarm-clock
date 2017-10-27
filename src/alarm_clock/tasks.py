bac808b133d03c90b7582180e03625ee04d4cd32
Stefan Hölzl <stefan.hoelzl@gmx.de>
2017-10-27 21:53:27 +0200

    first test with new alarm logic

--- src/alarm_clock/tasks.py
+++ src/alarm_clock/tasks.py
@@ -1,6 +1,5 @@
 import ntp
 import uasyncio as asyncio
-import utime as time
 
 from settings import TIME_ZONE
 
@@ -8,20 +7,28 @@ from alarm_clock.peripherials import daylight
 from alarm_clock.peripherials import switch
 from alarm_clock.peripherials import indicator
 
-async def alarm(alarm_time, daylight_forerun):
-    dt = alarm_time - time.time() - daylight_forerun
-    await asyncio.sleep(dt)
-    while alarm_time > time.time():
-        dt = alarm_time - time.time()
-        daylight.set(1 - (float(dt)/daylight_forerun))
+from alarm_clock.alarm import Alarm
+
+async def alarm_handler():
+    while True:
+        ringing = any((alarm.ringing for alarm in Alarm.all))
+        if ringing:
+            indicator.on()
+        else:
+            indicator.off()
+        dl = max((alarm.daylight for alarm in Alarm.all))
+        daylight.set(dl)
         await asyncio.sleep(1)
-    daylight.set(1)
-    indicator.on()
-    await switch
-    indicator.off()
-    daylight.set(0)
+
+async def off_switch():
+    while True:
+        await switch
+        for alarm in Alarm.all:
+            if alarm.ringing:
+                alarm.off()
+
 
 async def time_update():
     while True:
-        ntp.settime(tz=TIME_ZONE)
         await asyncio.sleep(3600)
+        ntp.settime(tz=TIME_ZONE)
--- src/app.py
+++ src/app.py
@@ -4,13 +4,22 @@ import utime as time
 
 from settings import TIME_ZONE
 from alarm_clock import peripherials
-from alarm_clock.tasks import time_update, alarm
+from alarm_clock.tasks import time_update, alarm_handler, off_switch
+from alarm_clock.alarm import Alarm
+
 
 ntp.settime(tz=TIME_ZONE)
 
+l = time.localtime()
+h = l[Alarm.HOUR]
+m = l[Alarm.MINUTE] + 1
+alarm = Alarm.activated(h, m, daylight_time=30)
+print("Time to ring:", (alarm.time - time.time()))
+
 loop = asyncio.get_event_loop()
 loop.create_task(time_update())
-loop.create_task(alarm(time.time()+15, 10))
+loop.create_task(alarm_handler())
+loop.create_task(off_switch())
 loop.run_forever()
 loop.close()
 
