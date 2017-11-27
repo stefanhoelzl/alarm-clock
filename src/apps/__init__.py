abdff8b17054cabe87dda18be90c7d56aa75fbca
Stefan Hölzl <stefan.hoelzl@gmx.de>
2017-11-27 19:09:22 +0100

    removed io_recorder

--- src/apps/__init__.py
+++ src/apps/__init__.py
@@ -3,6 +3,5 @@ from apps import ntp
 from apps import apify
 from apps import alarm_clock
 from apps import repl
-from apps import io_recorder
 from apps import gc
 from apps import alarm_clock_setter
