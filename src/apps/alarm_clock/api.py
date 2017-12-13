006aeb7a246135a26465ae4e66981c7e1b134a39
Stefan Hoelzl <stefan.hoelzl@baltech.de>
2017-12-13 13:17:05 +0100

    [feature] AlarmClockAPI to create/delete/disable/enable alarms and snooze/off them

--- src/apps/alarm_clock/api.py
+++ src/apps/alarm_clock/api.py
@@ -10,3 +10,42 @@ async def get_alarms(content):
     for aid, alarm in App.get_app("AlarmClockApp").alarms.items():
         ret[str(aid)] = alarm.as_dict()
     return ret
+
+
+@apify.route(b"/alarm/off")
+async def off(content):
+    App.get_app("AlarmClockApp").off()
+    return b""
+
+
+@apify.route(b"/alarm/snooze")
+async def snooze(content):
+    App.get_app("AlarmClockApp").snooze()
+    return b""
+
+
+@apify.route(b"/alarm/disable")
+async def disable(aid):
+    aid = int(aid)
+    App.get_app("AlarmClockApp").alarms[aid].disable()
+    return b""
+
+
+@apify.route(b"/alarm/enable")
+async def enable(aid):
+    aid = int(aid)
+    App.get_app("AlarmClockApp").alarms[aid].enable()
+    return b""
+
+
+@apify.route(b"/alarm/new")
+async def new(dct):
+    App.get_app("AlarmClockApp").create(**dct)
+    return b""
+
+
+@apify.route(b"/alarm/delete")
+async def delete(aid):
+    aid = int(aid)
+    del App.get_app("AlarmClockApp").alarms[aid]
+    return b""
---
+++ src/apps/alarm_clock/tests/test_api.py
@@ -0,0 +1,104 @@
+import pytest
+from unittest.mock import patch
+
+from ..api import *
+from ..alarm import Alarm
+from .. import AlarmClockApp
+from ..alarm_states import Disabled, Enabled
+
+
+class AlarmClockMock(AlarmClockApp):
+    def __init__(self, alarms=None, off=False, snooze=False):
+        super().__init__()
+        self.alarms = alarms if alarms is not None else {}
+        self._off = off
+        self._snooze = snooze
+
+    def off(self):
+        self._off = True
+
+    def snooze(self):
+        self._snooze = True
+
+
+@pytest.mark.asyncio
+async def test_getAlarms():
+    aid = 123
+    alarm = Alarm(h=7, m=35, days=(1, 2), snooze_time=50,
+                  daylight_mode=True, daylight_time=10)
+    with patch("uaos.App.get_app", return_value=AlarmClockMock({aid: alarm})):
+        alarms = (await get_alarms(""))
+    assert "123" in alarms
+    assert alarms["123"]['hour'] == 7
+    assert alarms["123"]['minute'] == 35
+    assert alarms["123"]['days'] == (1,2)
+    assert alarms["123"]['daylight_mode'] == True
+    assert alarms["123"]['daylight_time'] == 10
+    assert alarms["123"]['snooze_time'] == 50
+    assert alarms["123"]['daylight'] == 0
+    assert alarms["123"]['ringing'] is False
+    assert alarms["123"]['snoozing'] is False
+
+
+@pytest.mark.asyncio
+async def test_off():
+    ac = AlarmClockMock()
+    with patch("uaos.App.get_app", return_value=ac):
+        await off("")
+    assert ac.off
+
+
+@pytest.mark.asyncio
+async def test_snooze():
+    ac = AlarmClockMock()
+    with patch("uaos.App.get_app", return_value=ac):
+        await snooze("")
+    assert ac.snooze
+
+
+@pytest.mark.asyncio
+async def test_disable():
+    ac = AlarmClockMock({123: Alarm()})
+    assert ac.alarms[123] == Enabled
+    with patch("uaos.App.get_app", return_value=ac):
+        await disable("123")
+    assert ac.alarms[123] == Disabled
+
+
+@pytest.mark.asyncio
+async def test_enable():
+    ac = AlarmClockMock({123: Alarm()})
+    ac.alarms[123].disable()
+    assert ac.alarms[123] == Disabled
+    with patch("uaos.App.get_app", return_value=ac):
+        await enable("123")
+    assert ac.alarms[123] == Enabled
+
+
+@pytest.mark.asyncio
+async def test_new():
+    ac = AlarmClockMock()
+    with patch("uaos.App.get_app", return_value=ac):
+        await new({"hour": 7,
+                   "minute": 24,
+                   "days": (1, 2, 3),
+                   "daylight_mode": True,
+                   "daylight_time": 120,
+                   "snooze_time": 60
+                   })
+
+    assert ac.alarms[0] == Enabled
+    assert ac.alarms[0].hour == 7
+    assert ac.alarms[0].minute == 24
+    assert ac.alarms[0].days == (1, 2, 3)
+    assert ac.alarms[0].daylight_mode == True
+    assert ac.alarms[0].daylight_time == 120
+    assert ac.alarms[0].snooze_time == 60
+
+
+@pytest.mark.asyncio
+async def test_delete():
+    ac = AlarmClockMock({123: Alarm()})
+    with patch("uaos.App.get_app", return_value=ac):
+        await delete("123")
+    assert 123 not in ac.alarms
