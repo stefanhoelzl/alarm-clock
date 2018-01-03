8596c2b7621c29fc2784e78458c9e53fc48a3968
Stefan Hölzl <stefan.hoelzl@gmx.de>
2018-01-03 17:46:37 +0100

    [feature] Exception handling for AppServer

--- src/uaos/app.py
+++ src/uaos/app.py
@@ -10,7 +10,6 @@ class AppServer:
     running = []
     broken = {}
     finished = []
-    pending = []
 
     __execution_order__ = []
 
@@ -20,36 +19,40 @@ class AppServer:
             resolve_dependencies(AppServer.__registered__)
         AppServer.__execution_order__ = order
         for app_name in AppServer.__execution_order__:
-            try:
-                app = AppServer.__registered__[app_name]
-                app["instance"] = app["cls"]()
-                AppServer.initialized.append(app_name)
-            except Exception as e:
-                print("init failed", app_name, e)
-                AppServer.init_failed[app_name] = e
+            AppServer.setup_app(app_name)
+
+    @staticmethod
+    def setup_app(app_name):
+        try:
+            app = AppServer.__registered__[app_name]
+            app["instance"] = app["cls"]()
+            AppServer.initialized.append(app_name)
+        except Exception as e:
+            print("init failed", app_name, e)
+            AppServer.init_failed[app_name] = e
 
     @staticmethod
     async def call_safe(app_name, task):
-        try:
-            AppServer.pending.remove(app_name)
-            AppServer.running.append(app_name)
-            print("started", app_name)
-            await task()
-        except Exception as e:
-            print("broken", app_name, e)
-            AppServer.broken[app_name] = e
-        else:
-            print("finished", app_name)
-            AppServer.finished.append(app_name)
-        finally:
-            AppServer.running.remove(app_name)
+        while True:
+            try:
+                AppServer.running.append(app_name)
+                print("started", app_name)
+                await task()
+            except Exception as e:
+                print("broken", app_name, e)
+                AppServer.broken[app_name] = e
+            else:
+                print("finished", app_name)
+                AppServer.finished.append(app_name)
+                break
+            finally:
+                AppServer.running.remove(app_name)
 
     @staticmethod
     def start(loop):
         for app_name in AppServer.__execution_order__:
             infos = AppServer.__registered__[app_name]
             if hasattr(infos["instance"], '__call__'):
-                AppServer.pending.append(app_name)
                 loop.create_task(
                     AppServer.call_safe(
                         app_name, infos["instance"]
